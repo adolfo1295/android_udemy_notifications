634021803d3ce1d5c342e0f464225e5a7ec75b74
Adolfo Chavez <adolfo.dev295@gmail.com>
2017-09-11 15:25:44 -0500

    Commit final

--- app/src/main/java/notifications/curso/udemy/adolfo/com/notiications/MainActivity.java
+++ app/src/main/java/notifications/curso/udemy/adolfo/com/notiications/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
@@ -25,46 +26,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void createNotification(View v){
-        Intent intent = new Intent();
-        intent.setClass(this,NotificationActivity.class);
+        //se crea un intent, que sera la mochila de llevar los datos a la sig activity
+        Intent intent = new Intent(this, NotificationActivity.class);
+        Intent accion1 = new Intent(this, NotificationActivity.class);
+        accion1.putExtra("saludo","Hola humano!!");
+        //este pendingintent se colocara dentro de la notificacion para manejar lo que haga al darle click
+        //dentro de el ira el intent que creamos anteriormente
+        //PendingIntent.getActivity (context,requestcode,intent,flags)
         PendingIntent mPendingIntent = PendingIntent.getActivity(this,1,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent mPendingIntent1 = PendingIntent.getActivity(this,1,accion1,PendingIntent.FLAG_UPDATE_CURRENT);
+        //esto es para crear una notificacion
         NotificationCompat.Builder mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                //se le da un icono
                 .setSmallIcon(R.drawable.ic_moto)
+                //titulo
                 .setContentTitle("mi notificacion")
+                //texto o contenido d ela notificacion
                 .setContentText("creaste tu primer notificacion!!")
+                //aqui se coloca el pendingintent que mencionamos anteroormente
                 .setContentIntent(mPendingIntent)
+                //al darle click a la notificacion, se cancelara automaticamente
                 .setAutoCancel(true)
+                //se añade una accion, cabe destacar que en andorid 7.0 y superior ya no mostrara iconos
+                .addAction(R.drawable.ic_moto,"Saludo",mPendingIntent1)
+                //esto es para que vibre por default, haga un sonido por default y aparezca en la pantallaa
+                .setDefaults(Notification.DEFAULT_ALL)
+                //esto es para darle prioridad a la notificacion
                 .setPriority(Notification.PRIORITY_MAX);
+        mBuilder.setAutoCancel(true);
+        //con esto se cambia el color d ela notificacion
+        mBuilder.setColor(ContextCompat.getColor(getApplicationContext(),R.color.colorAccent));
         // Sets an ID for the notification
         int mNotificationId = 1;
         // Gets an instance of the NotificationManager service
         NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         // Builds the notification and issues it.
         mNotifyMgr.notify(mNotificationId, mBuilder.build());
-    }
-
-
-    public void buttonClicked(View v){
-        //build notification
-        Intent intent = new Intent();
-        intent.setClass(this,NotificationActivity.class);
-        PendingIntent mPendingIntent = PendingIntent.getActivity(this,1,intent,PendingIntent.FLAG_UPDATE_CURRENT);
-        NotificationCompat.Builder builder =
-                (NotificationCompat.Builder) new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.ic_moto)
-                        .setContentTitle("Ping Notification")
-                        .setContentText("Tomorrow will be your birthday.")
-                        .setAutoCancel(true)
-                        .setDefaults(Notification.DEFAULT_ALL) // must requires VIBRATE permission
-                        .setPriority(NotificationCompat.PRIORITY_HIGH) //must give priority to High, Max which will considered as heads-up notification
-                        .addAction(R.drawable.ic_moto,"accion 2",mPendingIntent)
-                        .addAction(R.mipmap.ic_launcher,"Titulo",mPendingIntent);
-
-        // Gets an instance of the NotificationManager service
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        //to post your notification to the notification bar with a id. If a notification with same id already exists, it will get replaced with updated information.
-        notificationManager.notify(0, builder.build());
     }
 
 }
--- app/src/main/java/notifications/curso/udemy/adolfo/com/notiications/NotificationActivity.java
+++ app/src/main/java/notifications/curso/udemy/adolfo/com/notiications/NotificationActivity.java
@@ -1,7 +1,10 @@
 package notifications.curso.udemy.adolfo.com.notiications;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class NotificationActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class NotificationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+        if (getIntent().getExtras()!=null){
+            String dato = getIntent().getExtras().getString("saludo",null);
+            Toast.makeText(this, dato, Toast.LENGTH_SHORT).show();
+        }
+        //al abrir la nueva actividad se vuelve a llamar a un notificationmanager
+        //para remover las notificaciones
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancel(1);
     }
 }
